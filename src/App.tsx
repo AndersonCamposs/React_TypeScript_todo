c14f5911bbca91279d6cc32cd77cdc8d7e0ee039
AndersonCamposs <anderson.camposs87@outlook.com>
2024-09-05 11:20:25 -0300

    Criação e estilização do formulário de criação de tarefas

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,23 @@ import styles from './App.module.css';
 // COMPONENTS
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas</h2>
+          <TaskList />
+        </div>
+      </main>
       <Footer />
     </div>
   );
