37673a09d520d9ff817bfe6059ee28d6024c2689
AndersonCamposs <anderson.camposs87@outlook.com>
2024-09-05 09:05:50 -0300

    Commit inicial

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+export default {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+      /* Isso desabilita as regras de formatação no ESLint que 
+      o Prettier será responsável por manipular. 
+      Certifique-se de que seja sempre a 
+      última configuração, para que tenha a chance de 
+      substituir outras configurações. */
+      'eslint-config-prettier',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh', 'import'],
+    rules: {
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  }
