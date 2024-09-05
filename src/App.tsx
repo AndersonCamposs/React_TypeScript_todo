cd39a282150c6058b4eac4480054e170da3ccb24
AndersonCamposs <anderson.camposs87@outlook.com>
2024-09-05 09:40:15 -0300

    Criação do componente header e footer e estilização inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import './App.css';
+import styles from './App.module.css';
+
+// COMPONENTS
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>React + TS</h1>
+      <Header />
+      <main className={styles.main}></main>
+      <Footer />
     </div>
   );
 }
